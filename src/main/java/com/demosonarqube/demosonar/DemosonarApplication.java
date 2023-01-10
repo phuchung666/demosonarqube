a030fab41730aafc338758b278aacd4d816be705
Hung Huynh <hung.hp666@gmail.com>
2023-01-10 21:21:34 +0700

    Initial commit

---
+++ src/main/java/com/demosonarqube/demosonar/DemosonarApplication.java
@@ -0,0 +1,13 @@
+package com.demosonarqube.demosonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demosonarqube/demosonar/DemosonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demosonarqube.demosonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
